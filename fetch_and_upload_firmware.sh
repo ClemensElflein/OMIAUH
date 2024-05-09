a99ebbcf364bcb80da6232b528145c258463830b
docgalaxyblock <potter-91@web.de>
2024-05-09 20:06:56 +0200

    feat: update flashing scripts
    fetch_and_upload_firmware.sh:
    - running as non-root check
    - check if running OpenMowerOS
    - check if OM_HW_VERSION is configured
    - implement firmware caching function:
        only download new fw if no cache exists or checksum differs
    - extracts only the required fw for flashing
    
    upload_firmware.sh:
    - check if running OpenMowerOS
    - set gpio un-/export on current state

--- fetch_and_upload_firmware.sh
+++ fetch_and_upload_firmware.sh
@@ -1,19 +1,80 @@
 #!/bin/bash
 
-source /boot/openmower/mower_config.txt
+# Force script to exit if an error occurs
+set -e
 
-# remove old firmware
-rm -rf firmware_download
-mkdir firmware_download
-cd firmware_download
 
-# Download the latest firmware
-echo "Fetching Firmware"
-wget https://github.com/ClemensElflein/OpenMower/releases/download/latest/firmware.zip
+SRC_DIR="$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")"
+FW_URL_BASE="https://github.com/ClemensElflein/OpenMower"
+FW_URL="$FW_URL_BASE/releases/download/latest/firmware"
 
-unzip firmware.zip
 
-cd ..
+if [[ ${EUID} -eq 0 ]]
+then
+    echo ""
+    echo "ERROR: !! THIS SCRIPT MUST NOT RUN AS ROOT !!"
+    echo "       It will ask for credentials as needed."
+    echo ""
+    exit 1
+fi
 
-echo "Flashing Firmware: ./firmware_download/firmware/$OM_HARDWARE_VERSION/firmware.elf"
-sudo ./upload_firmware.sh ./firmware_download/firmware/$OM_HARDWARE_VERSION/firmware.elf
+if [[ -f /etc/openmoweros_version ]]
+then
+    echo ""
+    echo " [✓] OpenMowerOS detected"
+    echo ""
+    source /boot/openmower/mower_config.txt
+else
+    echo ""
+    echo "ERROR: OpenMowerOS not detected!"
+    echo "This script is only supported on OpenMowerOS"
+    echo ""
+    exit 1
+fi
+
+if [[ -z "${OM_HARDWARE_VERSION}" ]]
+then
+    echo ""
+    echo "ERROR: "OM_HARDWARE_VERSION" is not specified"
+    echo "Please configure it at /boot/openmower/mower_config.txt before running this script again!"
+    echo ""
+    exit 1
+fi
+
+function download_fw() {
+  echo ""
+  echo "Downloading latest firmware.zip from \"$FW_URL_BASE\":"
+  wget --no-verbose --show-progress -O $HOME/firmware.zip ${FW_URL}.zip
+}
+
+
+if [[ -s $HOME/firmware.zip ]]
+then
+    echo ""
+    echo "Cached firmware.zip found."
+    echo "Comparing checksum with latest one from GitHub:"
+    echo ""
+    wget --no-verbose -O /tmp/firmware.sha256 ${FW_URL}.sha256
+    if sha256sum --check --status /tmp/firmware.sha256
+    then
+      echo " └> Cached firmware.zip hash matches."
+    else
+      echo " └> Cached firmware.zip hash differs."
+      download_fw
+    fi
+else
+    echo "No cached firmware.zip found."
+    download_fw
+fi
+
+FW_FILE="$HOME/firmware.elf"
+echo ""
+echo "Extrating firmware for \"$OM_HARDWARE_VERSION\""
+echo ""
+unzip -o -p $HOME/firmware.zip firmware/$OM_HARDWARE_VERSION/firmware.elf > $FW_FILE
+echo ""
+echo "Executing flash script with firmware \"$FW_FILE\":"
+sudo $SRC_DIR/upload_firmware.sh $FW_FILE
+
+echo ""
+echo " → Operation successfully completed 🎉"
--- upload_firmware.sh
+++ upload_firmware.sh
@@ -1,10 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script uploads new firmware to the Pico.
 # You need to provide the .elf file, not the .uf2
 # run as root!
 
-echo "10" > /sys/class/gpio/export
+# Force script to exit if an error occurs
+set -e
+
+if [[ -f /etc/openmoweros_version ]]
+then
+    echo ""
+    echo " [✓] OpenMowerOS detected"
+    echo ""
+else
+    echo ""
+    echo "ERROR: OpenMowerOS not detected!"
+    echo "This script is only supported on OpenMowerOS"
+    echo ""
+    exit 1
+fi
+
+if [[ ! -d /sys/class/gpio/gpio10/ ]]
+then
+  echo "10" > /sys/class/gpio/export
+fi
 echo "out" > /sys/class/gpio/gpio10/direction
 echo "1" > /sys/class/gpio/gpio10/value
 openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "program $1 verify reset exit"
+if [[ -d /sys/class/gpio/gpio10/ ]]
+then
+  echo "10" > /sys/class/gpio/unexport
+fi
